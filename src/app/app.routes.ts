73e5a3370eee255d4f7b46590a8d12fc8c5247f8
jvillacob <jvillacob@coorditanques.com>
2024-05-28 16:30:15 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './core/auth/components/login/login.component';
+import { RegisterComponent } from './core/auth/components/register/register.component';
+import { AuthComponent } from './core/auth/auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'auth/login', pathMatch: 'full'},
+  {
+    path: 'auth',
+    component: AuthComponent,
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: 'register',
+        component: RegisterComponent,
+      },
+    ],
+  },
+  { path: '**', redirectTo: 'auth/login', pathMatch: 'full' },
+];
---
+++ src/app/modules/finanzas/finanzas.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FinanzasRoutingModule } from './finanzas-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FinanzasRoutingModule
+  ]
+})
+export class FinanzasModule { }
